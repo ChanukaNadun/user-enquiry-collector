c60f3f0b93cede00d26b7d92cb8304bee0544c06
ChanuNadun <12345678+ChanukaNadun@users.noreply.github.com>
2025-09-05 23:49:41 +0530

    initial project

---
+++ EnquiryAPI/Model/EnquiryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EnquiryAPI.Model
+{
+    public class EnquiryDbContext:DbContext
+    {
+        public EnquiryDbContext(DbContextOptions<EnquiryDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
